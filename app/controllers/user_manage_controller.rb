6de1cad849bab30318c2149cba572cdd673b464c
ShoonLaeKhaing <shoonlaekhaing@mm.cybermissions.co.jp>
2024-04-23 10:51:22 +0900

    user権利

--- app/controllers/user_manage_controller.rb
+++ app/controllers/user_manage_controller.rb
@@ -1,9 +1,3 @@
-#Slack System
-#Direct Message Controller 
-#Authorname-NyiNyiMinThant@CyberMissions Myanmar Company limited 
-#@Since 27/06/2019
-#Version 1.0.0
-
 class UserManageController < ApplicationController
   def usermanage
     #check unlogin user
@@ -20,20 +14,18 @@ class UserManageController < ApplicationController
     @user_manages_activate = response['user_manages_activate']
     @user_manages_deactivate = response['user_manages_deactivate']
     @user_manages_admin = response['user_manages_admin']
-
-
-
+    
     #call from ApplicationController for retrieve home data
     retrievehome
   end
 
   def edit
-    MUser.where(id: params[:id]).update_all(member_status: 0)
+     get_data("/update?id=#{params[:id]}")
     redirect_to action:"usermanage"
   end
 
   def update
-    MUser.where(id: params[:id]).update_all(member_status: 1)
+    get_data("/update?id=#{params[:id]}")
     redirect_to action:"usermanage"
   end
 end
