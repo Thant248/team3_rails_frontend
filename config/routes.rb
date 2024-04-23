3c24069ba34744b904ca434b83e5e03b022c27f6
ShoonLaeKhaing <shoonlaekhaing@mm.cybermissions.co.jp>
2024-04-23 18:15:54 +0900

    app

--- config/routes.rb
+++ config/routes.rb
@@ -22,12 +22,6 @@ Rails.application.routes.draw do
 
   get 'channelcreate' => 'm_channels#new'
   post 'channelcreate' => 'm_channels#create'
-  get 'refresh_group' => 'm_channels#refresh_group'
-
-  get 'channeledit' => 'm_channels#edit'
-  get 'delete_channel' => 'm_channels#delete'
-  post 'channelupdate'=> 'm_channels#update'
-
 
   get 'refresh_direct' => 'm_users#refresh_direct'
 
@@ -47,14 +41,18 @@ Rails.application.routes.draw do
   get 'star' => 't_direct_star_msg#create'
   get 'unstar' => 't_direct_star_msg#destroy'
   get 'delete_directmsg' => "direct_message#deletemsg"
+
   get 'delete_directthread' => "direct_message#deletethread"
+  post 'directthreadmsg' => 'direct_message#showthread'
+  get 'starthread' => 't_direct_star_thread#create'
+  get 'unstarthread' => 't_direct_star_thread#destroy'
 
   # sessions
   delete 'logout' =>  'sessions#destroy'
 
   resources :m_workspaces, only: [:new, :create]
   resources :m_users, only: [:new, :create, :show, :refresh_direct]
-  resources :m_channels
+  resources :t_direct_messages
 
 
 
